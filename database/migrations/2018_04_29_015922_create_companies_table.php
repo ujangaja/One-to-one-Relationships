85f34f8592a63c61a1277313d3e2c8d49300c26d
Ujang Arisandi <ujangaja@gmail.com>
2018-04-29 09:09:20 +0700

    migration database

--- database/migrations/2018_04_29_015922_create_companies_table.php
+++ database/migrations/2018_04_29_015922_create_companies_table.php
@@ -15,6 +15,11 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
             $table->timestamps();
         });
     }
