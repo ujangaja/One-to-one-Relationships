5b9096cb3e34893325b71dd52cc3d17ccb659dfc
Ujang Arisandi <ujangaja@gmail.com>
2018-04-29 09:13:13 +0700

    model user dan company

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = ['user_id', 'name', 'address', 'phone'];
+
+    public function user()
+    {
+    	return $this->belongsTo('App/users', 'user_id');
+    }
 }
