f51e40ee5934d95e27375950ed9d278cbd5b3518
Ujang Arisandi <ujangaja@gmail.com>
2018-04-29 09:42:36 +0700

    membuat views, users/index.blade

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default">
+                    <div class="panel-heading">Users list</div>
+
+                    <div class="panel-body">
+                        <table class="table">
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Company Name</th>
+                                <th>Company Address</th>
+                                <th>Company Phone</th>
+                            </tr>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+			</div>
+		</div>
+	</div>
+@endsection
